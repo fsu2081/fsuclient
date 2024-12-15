d2f8fb00bcf6daf930d2fd0eecd20f0f017ecf1e
Sujan Ghimire <ghimire.amrit147@gmail.com>
2024-12-15 18:38:23 +0545

    events data bug fixed

--- pages/events.js
+++ pages/events.js
@@ -1,40 +1,27 @@
-import EventCard from '@/components/EventCard';
-import PastEvents from '@/components/PastEvents';
-import UpcomingEvents from '@/components/UpcomingEvents';
+import EventCard from "@/components/EventCard";
+import PastEvents from "@/components/PastEvents";
+import UpcomingEvents from "@/components/UpcomingEvents";
 // import { events } from '@/data/events';
-import React from 'react';
-import useSWR from 'swr';
+import React from "react";
+import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-const Events = ({ initialData }) => {
-  // console.log(initialData)
+const Events = () => {
   const { data: events } = useSWR(
     `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/admin/event`,
-    fetcher,
-    initialData
+    fetcher
   );
-  // console.log(events);
-
-  // console.log(events);
+  console.log(events);
 
   return (
     <div className="w-full min-h-[49rem] flex justify-center ">
       <div className="w-[90%] py-16 flex flex-col gap-8">
-        {events?.upcomingEvents && (
-          <UpcomingEvents events={events?.upcomingEvents} />
-        )}
-        <PastEvents events={events?.pastEvents} />
+        {events && <UpcomingEvents events={events} />}
+        <PastEvents events={events} />
       </div>
     </div>
   );
 };
 
 export default Events;
-
-export async function getServerSideProps() {
-  const initialData = await fetcher(
-    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/admin/event`
-  );
-  return { props: { initialData } };
-}
